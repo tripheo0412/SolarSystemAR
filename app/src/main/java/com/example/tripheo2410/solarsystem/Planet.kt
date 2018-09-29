e593b8f7ba78b63238e0503b30d833d8981691f6
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-29 14:57:18 +0300

    add func onUpdate

--- app/src/main/java/com/example/tripheo2410/solarsystem/Planet.kt
+++ app/src/main/java/com/example/tripheo2410/solarsystem/Planet.kt
@@ -29,7 +29,24 @@ class Planet(
 
 
 
+    override fun onUpdate(frameTime: FrameTime?) {
+        if (infoCard == null) {
+            return
+        }
 
+        // Typically, getScene() will never return null because onUpdate() is only called when the node
+        // is in the scene.
+        // However, if onUpdate is called explicitly or if the node is removed from the scene on a
+        // different thread during onUpdate, then getScene may be null.
+        if (scene == null) {
+            return
+        }
+        val cameraPosition = scene.camera.worldPosition
+        val cardPosition = infoCard!!.worldPosition
+        val direction = Vector3.subtract(cameraPosition, cardPosition)
+        val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
+        infoCard!!.worldRotation = lookRotation
+    }
 
     override fun onTap(hitTestResult: HitTestResult?, motionEvent: MotionEvent?) {
         if (infoCard == null) {
