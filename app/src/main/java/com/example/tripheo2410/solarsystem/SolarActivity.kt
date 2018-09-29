10e5c0a427b7effdcead911a936bc6460187cf98
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-29 14:10:35 +0300

    initial commit

---
+++ app/src/main/java/com/example/tripheo2410/solarsystem/SolarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tripheo2410.solarsystem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SolarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solar)
+    }
+}
