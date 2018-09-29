cf8e14993376f3011f16eccf95553643bd710407
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-29 15:20:41 +0300

    render arview to screen

--- app/src/main/java/com/example/tripheo2410/solarsystem/SolarActivity.kt
+++ app/src/main/java/com/example/tripheo2410/solarsystem/SolarActivity.kt
@@ -1,8 +1,10 @@
 package com.example.tripheo2410.solarsystem
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
@@ -14,10 +16,12 @@ import com.google.ar.core.exceptions.CameraNotAvailableException
 import com.google.ar.core.exceptions.UnavailableException
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.HitTestResult
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.ViewRenderable
+import java.util.concurrent.CompletableFuture
 
 class SolarActivity : AppCompatActivity() {
     private var installRequested: Boolean = false
@@ -55,6 +59,119 @@ class SolarActivity : AppCompatActivity() {
             return
         }
         setContentView(R.layout.activity_solar)
+        arSceneView = findViewById(R.id.ar_scene_view)
+
+        // Build all the planet models.
+        val sunStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Sol.sfb")).build()
+        val mercuryStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Mercury.sfb")).build()
+        val venusStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Venus.sfb")).build()
+        val earthStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Earth.sfb")).build()
+        val lunaStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Luna.sfb")).build()
+        val marsStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Mars.sfb")).build()
+        val jupiterStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Jupiter.sfb")).build()
+        val saturnStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Saturn.sfb")).build()
+        val uranusStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Uranus.sfb")).build()
+        val neptuneStage : CompletableFuture<ModelRenderable> = ModelRenderable.builder().setSource(this, Uri.parse("Neptune.sfb")).build()
+
+        // Build a renderable from a 2D View.
+
+        CompletableFuture.allOf(
+                sunStage,
+                mercuryStage,
+                venusStage,
+                earthStage,
+                lunaStage,
+                marsStage,
+                jupiterStage,
+                saturnStage,
+                uranusStage,
+                neptuneStage)
+                .handle<Any> { notUsed, throwable ->
+                    // When you build a Renderable, Sceneform loads its resources in the background while
+                    // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                    // before calling get().
+
+                    if (throwable != null) {
+                        DemoUtils.displayError(this, "Unable to load renderable", throwable)
+                        return@handle null
+                    }
+
+                    try {
+                        sunRenderable = sunStage.get()
+                        mercuryRenderable = mercuryStage.get()
+                        venusRenderable = venusStage.get()
+                        earthRenderable = earthStage.get()
+                        lunaRenderable = lunaStage.get()
+                        marsRenderable = marsStage.get()
+                        jupiterRenderable = jupiterStage.get()
+                        saturnRenderable = saturnStage.get()
+                        uranusRenderable = uranusStage.get()
+                        neptuneRenderable = neptuneStage.get()
+                        // Everything finished loading successfully.
+                        hasFinishedLoading = true
+
+                    } catch (ex: InterruptedException) {
+                        DemoUtils.displayError(this, "Unable to load renderable", ex)
+                    }
+
+                    null
+                }
+
+        // Set up a tap gesture detector.
+        gestureDetector = GestureDetector(
+                this,
+                object : GestureDetector.SimpleOnGestureListener() {
+                    override fun onSingleTapUp(e: MotionEvent): Boolean {
+                        onSingleTap(e)
+                        return true
+                    }
+
+                    override fun onDown(e: MotionEvent): Boolean {
+                        return true
+                    }
+                })
+
+        // Set a touch listener on the Scene to listen for taps.
+        arSceneView!!
+                .scene
+                .setOnTouchListener { hitTestResult: HitTestResult, event: MotionEvent ->
+                    // If the solar system hasn't been placed yet, detect a tap and then check to see if
+                    // the tap occurred on an ARCore plane to place the solar system.
+                    if (!hasPlacedSolarSystem) {
+                        return@setOnTouchListener gestureDetector !!. onTouchEvent (event)
+                    }
+
+                    // Otherwise return false so that the touch event can propagate to the scene.
+                    false
+                }
+
+        // Set an update listener on the Scene that will hide the loading message once a Plane is
+        // detected.
+        arSceneView!!
+                .scene
+                .addOnUpdateListener { frameTime ->
+                    if (loadingMessageSnackbar == null) {
+                        return@addOnUpdateListener
+                    }
+
+                    val frame = arSceneView!!.arFrame
+                    if (frame == null) {
+                        return@addOnUpdateListener
+                    }
+
+                    if (frame!!.camera.trackingState != TrackingState.TRACKING) {
+                        return@addOnUpdateListener
+                    }
+
+                    for (plane in frame.getUpdatedTrackables<Plane>(Plane::class.java)) {
+                        if (plane.trackingState == TrackingState.TRACKING) {
+                            hideLoadingMessage()
+                        }
+                    }
+                }
+
+        // Lastly request CAMERA permission which is required by ARCore.
+        DemoUtils.requestCameraPermission(this, RC_PERMISSIONS)
     }
 
     override fun onResume() {
