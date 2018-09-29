b7af087bb789ac475d455ba78603c5a8508ce242
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-29 14:56:48 +0300

    add func onTap

--- app/src/main/java/com/example/tripheo2410/solarsystem/Planet.kt
+++ app/src/main/java/com/example/tripheo2410/solarsystem/Planet.kt
@@ -1,2 +1,46 @@
 package com.example.tripheo2410.solarsystem
 
+import android.content.Context
+import android.view.MotionEvent
+import android.widget.TextView
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.HitTestResult
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+
+class Planet(
+        private val context: Context,
+        private val planetName: String,
+        private val planetScale: Float,
+        private val planetRenderable: ModelRenderable,
+        private val solarSettings: SolarSettings) : Node(), Node.OnTapListener {
+
+    private var infoCard: Node? = null
+
+
+
+    init {
+        setOnTapListener(this)
+    }
+
+
+
+
+
+
+    override fun onTap(hitTestResult: HitTestResult?, motionEvent: MotionEvent?) {
+        if (infoCard == null) {
+            return
+        }
+
+        infoCard!!.isEnabled = !infoCard!!.isEnabled
+    }
+
+    companion object {
+
+        private val INFO_CARD_Y_POS_COEFF = 0.55f
+    }
+}
--- app/src/main/java/com/example/tripheo2410/solarsystem/SolarSettings.kt
+++ app/src/main/java/com/example/tripheo2410/solarsystem/SolarSettings.kt
@@ -1,2 +1,6 @@
 package com.example.tripheo2410.solarsystem
 
+class SolarSettings {
+    var orbitSpeedMultiplier = 1.0f
+    var rotationSpeedMultiplier = 1.0f
+}
